5920871cbce27f7d2658bf6d61b6282f83d213de
Viktor Peredera <vitia2282@gmail.com>
2024-04-15 14:21:35 +0300

    feat: replace docs with updated e-mail

--- src/components/static-pages-components/DownloadScanButton.tsx
+++ src/components/static-pages-components/DownloadScanButton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function DownloadScanButton({ href, fileName }: { href: string, fileName: string }) {
   return <div className="mt-10 mb-6 md:mb-[80px] p-5 bg-tertiary-bg flex flex-col md:flex-row justify-between items-center gap-2">
-    <span className="font-semibold text-16 sm:text-18 md:text-24 ">Download scan of document</span>
+    <span className="font-semibold text-16 sm:text-18 md:text-24 ">Download document</span>
     <a download={fileName} href={href} className="border border-green px-6 py-3 flex gap-2 items-center rounded-1 hover:text-green duration-200">
       Download PDF file
       <Svg iconName="download"/>
